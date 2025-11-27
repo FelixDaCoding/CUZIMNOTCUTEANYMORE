edec645e104b1c586665362e13636c66c428c2b8
Joseph Xeno Banzon <xenoako@gmail.com>
2025-11-28 00:54:31 +0800

    Enhance Quest class with new attributes and methods
    Added attributes and methods to manage quest completion and rewards.

--- Capstone_LevelUP/src/Quest.java
+++ Capstone_LevelUP/src/Quest.java
@@ -1,24 +1,54 @@
 import java.time.LocalDateTime;
 
 public class Quest {
-    // Attributes
     private String questId;
     private String questName;
     private int rewardXP;
-    private String questType; // "Weekly", "Special"
+    private String questType; // "LevelUp", "Streak", "WorkoutCount"
+    private int targetAmount; // Generic target (e.g., Level 5, Streak 10)
     private LocalDateTime timeLimit;
+    private boolean isClaimed;
 
-    // Methods from Diagram
+    public Quest(String name, int reward, String type, int target) {
+        this.questName = name;
+        this.rewardXP = reward;
+        this.questType = type;
+        this.targetAmount = target;
+        this.isClaimed = false;
+        this.timeLimit = LocalDateTime.now().plusDays(7); // Default 1 week
+    }
+
     public boolean checkCompletion(User user) {
-        // TODO: Validate requirements
-        return false;
+        if (isClaimed) return true;
+
+        boolean conditionMet = false;
+        
+        switch (questType) {
+            case "LevelUp":
+                conditionMet = user.getLevel() >= targetAmount;
+                break;
+            case "Streak":
+                conditionMet = user.getStreak().getCurrentStreak() >= targetAmount;
+                break;
+             // Add more cases as needed
+        }
+        
+        return conditionMet;
     }
 
     public void claimReward(User user) {
-        // TODO: Give XP to user
+        if (!isClaimed && checkCompletion(user)) {
+            // Since User doesn't have a public addXP, we assume logWorkout handles it
+            // or we print for now. Ideally User has public void gainXP(int amount).
+            System.out.println("Quest Claimed! +" + rewardXP + " XP");
+            isClaimed = true;
+        }
     }
 
     public void resetQuest() {
-        // TODO: Reset progress counters
+        this.isClaimed = false;
     }
+    
+    public String getQuestName() { return questName; }
+    public int getRewardXP() { return rewardXP; }
 }
